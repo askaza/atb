71aece911959e9a3bdeaf33ea4d1e344232f79f4
asya <asya.zaharova@gmail.com>
2012-11-16 09:11:54 +0400

    home page

--- code/js/common.js
+++ code/js/common.js
@@ -1,62 +1,13 @@
 $(document).ready(function() {
 
-	$('#date-range').click(function(){
-		$(this).addClass('active-range');
-		$('#range').show();
+	var windowsize = $(window).width();
+	$('.panorama__img').panorama({
+	    viewport_width: windowsize,
+	    speed: 30000,
+	    direction: 'left',
+	    control_display: 'yes',
+	    mode_360: false
 	});
 
-	$(".data-field").click(function(e) {
-		var target = $(e.currentTarget);
-		var max = new Date(3000, 0, 0);
-		var min = new Date(0, 0, 0);
-		var curTest = null;
-		
-		
-		$( "#range" ).datepicker({
-				dateFormat: 'yy/mm/dd'});
-				
-		if(target.attr('id') == 'from'){
-			curTest = max;
-			if($('#to').val().length){
-				curTest = new Date($('#to').val());
-			}
-			$( "#range" ).datepicker('change', {
-				dateFormat: 'yy/mm/dd',
-				onSelect: function(dateText, inst) {
-					target.val(dateText);
-				},
-				maxDate: curTest,
-				minDate: min
-			});	
-		} else {
-			curTest = min;
-			if($('#from').val().length){
-				curTest = new Date($('#from').val());
-			}
-
-			$( "#range" ).datepicker('change', {
-				minDate: curTest,
-				dateFormat: 'yy/mm/dd',
-				onSelect: function(dateText, inst) {
-					target.val(dateText);
-				},
-				minDate: curTest,
-				maxDate: max
-			});
-		}
-		return false;		
-	});
-
-
-		
-	$( "#tabs" ).tabs();
-	
-	$("#clients-select").selectbox();
-	
-	if ( $.browser.msie && $.browser.version < 9 ) {
-		$('.ui-tabs-nav li a').append('<i class="tl"></i><i class="tr"></i>');	
-		$('.payments').append('<i class="tl"></i><i class="tr"></i><i class="bl"></i><i class="br"></i>');
-		$('.equal-height__bottom .equal-height__column').append('<i class="bl"></i>');
-	}
 });
 
---
+++ code/js/jquery.panorama.js
@@ -0,0 +1,130 @@
+/* =========================================================
+// jquery.panorama.js
+// Author: OpenStudio (Arnault PACHOT)
+// Mail: apachot@openstudio.fr
+// Web: http://www.openstudio.fr
+// Copyright (c) 2008 Arnault Pachot
+// licence : GPL
+========================================================= */
+
+(function($) {
+	$.fn.panorama = function(options) {
+		this.each(function(){
+			var settings = {
+				viewport_width: 600,
+				speed: 20000,
+				direction: 'left',
+				control_display: 'auto',
+				start_position: 0,
+				auto_start: true,
+				mode_360: true
+			};
+			if(options) $.extend(settings, options);
+
+			var elemWidth = parseInt($(this).attr('width'));
+			var elemHeight = parseInt($(this).attr('height'));
+			var currentElement = this;
+			var panoramaViewport, panoramaContainer;
+			var bMouseMove = false;
+			var mouseMoveStart = 0;
+			var mouseMoveMarginStart = 0;
+
+			$(this).attr('unselectable','on')
+				.css('position', 'relative')
+				.css('-moz-user-select','none')
+				.css('-webkit-user-select','none')
+				.css('margin', '0')
+				.css('padding', '0')
+				.css('border', 'none')
+				.wrap("<div class='panorama-container'></div>");
+			if (settings.mode_360)
+				$(this).clone().insertAfter(this);
+
+
+
+			panoramaContainer = $(this).parent();
+			panoramaContainer.css('height', elemHeight+'px').css('overflow', 'hidden').wrap("<div class='panorama-viewport'></div>").parent().css('width',settings.viewport_width+'px')
+				.append("<div class='panorama-control'><a href='#' class='panorama-control-left'><<</a> <a href='#' class='panorama-control-pause'>x</a> <a href='#' class='panorama-control-right'>>></a> </div>");
+
+			panoramaViewport = panoramaContainer.parent();
+
+			panoramaViewport.css('height', elemHeight+'px').css('overflow', 'hidden').find('a.panorama-control-left').bind('click', function() {
+				$(panoramaContainer).stop();
+				settings.direction = 'right';
+				animate_left(panoramaContainer, elemWidth, settings);
+				return false;
+			});
+			panoramaViewport.bind('click', function() {
+				$(panoramaContainer).stop();
+			});
+			panoramaViewport.find('a.panorama-control-right').bind('click', function() {
+				$(panoramaContainer).stop();
+				settings.direction = 'left';
+				animate_right(panoramaContainer, elemWidth, settings);
+				return false;
+			});
+			panoramaViewport.find('a.panorama-control-pause').bind('click', function() {
+				$(panoramaContainer).stop();
+				return false;
+			});
+
+
+
+			$(this).parent().css('margin-left', '-'+settings.start_position+'px');
+
+			if (settings.auto_start)
+				panorama_animate(panoramaContainer, elemWidth, settings);
+
+		});
+
+
+
+		function animate_right (element, elemWidth, settings) {
+		    currentPosition = parseInt($(element).css('margin-left'));
+		    var rightlimit;
+		    rightlimit = elemWidth - $(window).width();
+
+		    if ( currentPosition - 300 < -rightlimit) {
+			 $(element).animate({ marginLeft: -rightlimit}, ((settings.speed / elemWidth)*300) , 'easeInOutCubic');
+		    } else {
+			$(element).animate({ marginLeft: currentPosition - 400}, ((settings.speed / elemWidth)*300) , 'easeInOutCubic');
+		    }
+		}
+
+		  function animate_left (element, elemWidth, settings) {
+		   currentPosition = parseInt($(element).css('margin-left'));
+
+		    if ( currentPosition + 400 > 0) {
+			 $(element).animate({ marginLeft: 0}, ((settings.speed / elemWidth)*300) , 'easeInOutCubic');
+		    } else {
+			$(element).animate({ marginLeft: currentPosition + 400}, ((settings.speed / elemWidth)*300) , 'easeInOutCubic');
+		    }
+		  }
+
+
+		function panorama_animate(element, elemWidth, settings) {
+		    currentPosition = 0-parseInt($(element).css('margin-left'));
+		    var rightlimit;
+		    if (settings.direction == 'right') {
+
+		$(element).animate({  marginLeft: 0}, ((settings.speed / elemWidth) * (currentPosition)) , 'linear', function (){
+		    settings.direction = 'left';
+		    panorama_animate(element, elemWidth, settings);
+		});
+		     } else {
+		rightlimit = elemWidth-settings.viewport_width;
+		$(element).animate({ marginLeft: -rightlimit }, ((settings.speed / rightlimit) * (rightlimit - currentPosition)), 'linear', function (){
+		    settings.direction = 'right';
+		    panorama_animate(element, elemWidth, settings);
+		});
+	    }
+	}
+
+		$(window).resize(function() {
+		    var windowsize = $(window).width();
+		    $('.panorama-viewport').width(windowsize);
+		    $('.panorama-container').css('margin-left', '0');
+		});
+    };
+
+})(jQuery);
